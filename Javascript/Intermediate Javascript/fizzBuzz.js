b4f60eb77b83cd76076d617fe208337b78343f52
Midhun Mathew <midmat4828@gmail.com>
2024-10-13 21:41:24 +0530

    test

--- Javascript/Intermediate Javascript/fizzBuzz.js
+++ Javascript/Intermediate Javascript/fizzBuzz.js
@@ -1,21 +1,40 @@
 var i = 1;
-var result;
+// var result;
 var arr = [];
 
-for (i = 1; i <= 100; i++) {
-    if (i % 3 == 0 && i % 5 == 0) {
-        result = "FizzBuzz";
+// for (i = 1; i <= 100; i++) {
+//     if (i % 3 == 0 && i % 5 == 0) {
+//         result = "FizzBuzz";
 
+//     }
+//     else if (i % 3 == 0) {
+//         result = "Fizz";
+//     } else if (i % 5 == 0) {
+//         result = "Buzz"
+//     } else {
+//         result = i;
+//     }
+
+//     arr.push(result);
+// }
+
+// console.log(arr)
+
+function fizzBuzz() {
+    // arr.push(i);
+    console.log(arr);
+
+    if (i % 3 == 0 && i % 5 == 0) {
+        arr.push("FizzBuzz");
     }
     else if (i % 3 == 0) {
-        result = "Fizz";
+        arr.push("Fizz");
     } else if (i % 5 == 0) {
-        result = "Buzz"
+        arr.push("Buzz");
+
     } else {
-        result = i;
+        arr.push(i);
     }
+    i++;
 
-    arr.push(result);
 }
-
-console.log(arr)
