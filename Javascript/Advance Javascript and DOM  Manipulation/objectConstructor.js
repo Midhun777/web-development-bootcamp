0397f3c4cf88a3a8c2a41649f32555aedf4f1e2f
Midhun Mathew <midmat4828@gmail.com>
2024-10-16 18:52:27 +0530

    test

--- Javascript/Advance Javascript and DOM  Manipulation/objectConstructor.js
+++ Javascript/Advance Javascript and DOM  Manipulation/objectConstructor.js
@@ -1,10 +1,14 @@
 
-function EmpDetails(name, desig, salary, lang) {
+function EmpDetails(name, desig, salary, lang,doWork) {
     this.name = name;
     this.desig = desig;
     this.salary = salary;
     this.lang = lang;
+    this.doWork = function(){
+        console.log("Doing Work!")
+    }
 }
 
 var employee = new EmpDetails("John", "HRM", 12000, ["English", "Mlayalam"]);
 console.log(employee)
+employee.doWork();
