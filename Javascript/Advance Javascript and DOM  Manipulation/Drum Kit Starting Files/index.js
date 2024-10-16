855e6ac52e6a13b4c31520043bd73cd321a95da0
Midhun Mathew <midmat4828@gmail.com>
2024-10-16 20:12:33 +0530

    test

--- Javascript/Advance Javascript and DOM  Manipulation/Drum Kit Starting Files/index.js
+++ Javascript/Advance Javascript and DOM  Manipulation/Drum Kit Starting Files/index.js
@@ -1,48 +1,48 @@
 var numOfBtn = document.querySelectorAll(".drum").length;
-var keyName;
 
-function keyPressFunc(e) {
-    keyName = e.code.slice(3, 4).toLowerCase();
-    console.log(keyName);
-}
-window.addEventListener("keydown", keyPressFunc);
+window.addEventListener("keydown", function (e) {
+    var keyName = e.code.slice(3, 4).toLowerCase();
+    makeSound(keyName);
+});
 
 for (var i = 0; i <= numOfBtn; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-
-
         var buttonName = this.innerHTML;
-        console.log(buttonName);
-        switch (buttonName) {
-            case "w":
-                var tom1 = new Audio("sounds/tom-1.mp3");
-                tom1.play();
-                break;
-            case "a":
-                var tom2 = new Audio("sounds/tom-2.mp3");
-                tom2.play();
-                break;
-            case "s":
-                var tom3 = new Audio("sounds/tom-3.mp3");
-                tom3.play();
-                break;
-            case "d":
-                var tom4 = new Audio("sounds/tom-4.mp3");
-                tom4.play();
-                break;
-            case "j":
-                var crash = new Audio("sounds/crash.mp3");
-                crash.play();
-                break;
-            case "k":
-                var kick = new Audio("sounds/kick-bass.mp3");
-                kick.play();
-                break;
-            case "l":
-                var snare = new Audio("sounds/snare.mp3");
-                snare.play();
-                break;
-        }
+        makeSound(buttonName);
     })
 }
 
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "k":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        case "l":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        default:
+            console.error("Invalid Key Pressed!")
+    }
+}
