3cf70b947f35efddf9b6a5c2284e2bd3c9ce81a1
Midhun Mathew <midmat4828@gmail.com>
2024-10-17 23:18:56 +0530

    node js

---
+++ Node js/fileSystem.js
@@ -0,0 +1,12 @@
+const fs = require('node:fs');
+
+const content = 'Some content!';
+
+fs.writeFile('test.txt', content, err => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log("sucessfully written!")
+
+    }
+});
