4c753c39aadc5961982c289d98bbb88e9b15dea2
Midhun Mathew <midmat4828@gmail.com>
2024-10-18 22:17:22 +0530

    node

--- Node-js/QR-Code-node/index.js
+++ Node-js/QR-Code-node/index.js
@@ -1,22 +1,9 @@
-/* 
-1. Use the inquirer npm package to get user input.
-2. Use the qr-image npm package to turn the user entered URL into a QR code image.
-3. Create a txt file to save the user input using the native fs node module.
-*/
-
-// import fs from 'inquirer';
-
-// fs.readFile('./URL.txt', 'utf8', (err, data) => {
-//     if (err) {
-//         console.log(err);
-//         return;
-//     }
-//     console.log(data);
-// });
+import fs from 'node:fs';
+import inquirer from 'inquirer';
+import qr from 'qr-image';
+import { createWriteStream } from 'fs';
 
 let url;
-
-import fs from 'node:fs';
 
 fs.readFile('./URL.txt', 'utf8', (err, data) => {
     if (err) {
@@ -26,9 +13,6 @@ fs.readFile('./URL.txt', 'utf8', (err, data) => {
     console.log(data);
     url = data;
 });
-
-
-import inquirer from 'inquirer';
 
 inquirer
     .prompt([
@@ -45,23 +29,9 @@ inquirer
         }
     });
 
-// var qr = require('qr-image');
-
-// import qr from 'qr-image';
-
-// var qr_svg = qr.image('I love QR!', { type: 'svg' });
-// qr_svg.pipe(require('fs').createWriteStream('i_love_qr.svg'));
-
-// var svg_string = qr.imageSync('I love QR!', { type: 'svg' });
-
-import qr from 'qr-image';
-import { createWriteStream } from 'fs';
-
-// Generate QR code and write it to a file
 const qr_svg = qr.image("midhun777.vercel.app", { type: 'png' });
 qr_svg.pipe(createWriteStream('i_love_qr.png'));
 
-// Generate QR code and return it as an SVG string
 const svg_string = qr.imageSync('I love QR!', { type: 'png' });
 
 console.log('QR code generated and saved as i_love_qr.png');
