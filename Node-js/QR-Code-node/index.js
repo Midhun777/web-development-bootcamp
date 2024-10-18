b3aa278c50974aa585fa0aef9ff83f9fbfcdeb5e
Midhun Mathew <midmat4828@gmail.com>
2024-10-18 22:49:32 +0530

    QR Code node js

--- Node-js/QR-Code-node/index.js
+++ Node-js/QR-Code-node/index.js
@@ -1,38 +1,23 @@
-import fs from 'node:fs';
+import fs from 'fs';
 import inquirer from 'inquirer';
 import qr from 'qr-image';
-import { createWriteStream } from 'fs';
-
-let url;
-
-fs.readFile('./URL.txt', 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(data);
-    url = data;
-});
 
 inquirer
-    .prompt([
-        console.log(url)
-    ])
+    .prompt([{
+        message: "Type your URL",
+        name: "URL"
+    }])
     .then((answers) => {
-        console.log(answers);
+        const url = answers.URL;
+        console.log(url);
+
+        var qr_svg = qr.image(url);
+        qr_svg.pipe(fs.createWriteStream('qr_image.png'));
     })
     .catch((error) => {
         if (error.isTtyError) {
             console.log(error)
         } else {
-            console.log("Data Sucessfully Imported From Text File.")
+            console.log("Data sucessfully imported from text file and generated qr-code.")
         }
     });
-
-const qr_svg = qr.image("midhun777.vercel.app", { type: 'png' });
-qr_svg.pipe(createWriteStream('i_love_qr.png'));
-
-const svg_string = qr.imageSync('I love QR!', { type: 'png' });
-
-console.log('QR code generated and saved as i_love_qr.png');
-
