43ea7f480f8e4d108323ef6a42bd6d4cbf84ed4e
Midhun Mathew <midmat4828@gmail.com>
2024-10-18 22:11:12 +0530

    node

--- Node-js/QR-Code-node/index.js
+++ Node-js/QR-Code-node/index.js
@@ -14,6 +14,7 @@
 //     console.log(data);
 // });
 
+let url;
 
 import fs from 'node:fs';
 
@@ -23,6 +24,7 @@ fs.readFile('./URL.txt', 'utf8', (err, data) => {
         return;
     }
     console.log(data);
+    url = data;
 });
 
 
@@ -30,17 +32,37 @@ import inquirer from 'inquirer';
 
 inquirer
     .prompt([
-        console.log(data)
-
+        console.log(url)
     ])
     .then((answers) => {
-        console.log(answers)
-
+        console.log(answers);
     })
     .catch((error) => {
         if (error.isTtyError) {
             console.log(error)
         } else {
-            console.log("idk error")
+            console.log("Data Sucessfully Imported From Text File.")
         }
     });
+
+// var qr = require('qr-image');
+
+// import qr from 'qr-image';
+
+// var qr_svg = qr.image('I love QR!', { type: 'svg' });
+// qr_svg.pipe(require('fs').createWriteStream('i_love_qr.svg'));
+
+// var svg_string = qr.imageSync('I love QR!', { type: 'svg' });
+
+import qr from 'qr-image';
+import { createWriteStream } from 'fs';
+
+// Generate QR code and write it to a file
+const qr_svg = qr.image("midhun777.vercel.app", { type: 'png' });
+qr_svg.pipe(createWriteStream('i_love_qr.png'));
+
+// Generate QR code and return it as an SVG string
+const svg_string = qr.imageSync('I love QR!', { type: 'png' });
+
+console.log('QR code generated and saved as i_love_qr.png');
+
