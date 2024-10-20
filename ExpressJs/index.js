f437461a73ca1e323fc0c57f34d8b0fb74db72d7
Midhun Mathew <midmat4828@gmail.com>
2024-10-20 13:45:30 +0530

    middleware

--- ExpressJs/index.js
+++ ExpressJs/index.js
@@ -1,11 +1,27 @@
 import express from "express"
 const app = express();
-const port = 3000;
-
-app.get("/", (req, res) => {
-    res.send("Hello from the ExpressJs!");
-});
+const port = 5500;
 
 app.listen(port, () => {
-    console.log(`Server listning on port ${port}!`);
-})
+    console.log(`Listening on ${port}`)
+});
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello from EJS.</h1>")
+});
+
+app.post("/about", (req, res) => {
+    res.sendStatus(201);
+    console.log(res);
+});
+
+app.put("/user/midhun", (req, res) => {
+    res.sendStatus(200);
+    console.log(res);
+});
+
+app.patch("/blog", (req, res) => {
+    res.sendStatus(200);
+    console.log(res);
+});
+
