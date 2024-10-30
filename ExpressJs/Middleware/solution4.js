55575df8db7338e7ede5b491816888a4cf409bcf
Midhun Mathew <midmat4828@gmail.com>
2024-10-30 15:31:11 +0530

    test

--- ExpressJs/Middleware/solution4.js
+++ ExpressJs/Middleware/solution4.js
@@ -31,9 +31,3 @@ app.post("/submit", (req, res) => {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
