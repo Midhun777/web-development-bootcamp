6dc1ee5bb864751b80ab266491020045ed39f31e
Midhun Mathew <midmat4828@gmail.com>
2024-10-31 17:32:16 +0530

    EJS

--- ExpressJs/Middleware/index3.js
+++ ExpressJs/Middleware/index3.js
@@ -3,9 +3,15 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+function logger(req, res, next) {
+  console.log("Request Method : ", req.method);
+  console.log("Request URL : ", req.url);
+  next();
+}
+
 app.use(logger);
 
-app.get("/", (req, res) => {
+app.get("/", (req, req) => {
   res.send("Hello");
 });
 
