53bd554725b41337c95a5ff5092c8e026db26592
Midhun Mathew <midmat4828@gmail.com>
2024-11-04 21:32:37 +0530

    test

--- EJS/index.js
+++ EJS/index.js
@@ -11,3 +11,11 @@ app.listen(PORT,()=>{
 
 const dayName = new Date();
 const day = dayName.getDay()
+const dayName = new Date();
+const day = dayName.getDay()
+const dayName = new Date();
+const day = dayName.getDay()
+const dayName = new Date();
+const day = dayName.getDay()
+const dayName = new Date();
+const day = dayName.getDay()
