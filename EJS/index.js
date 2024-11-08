57f73295fd4e6566f4aaafa7e71cceeae1bcd7ab
Midhun Mathew <midmat4828@gmail.com>
2024-11-08 18:40:34 +0530

    TEST

--- EJS/index.js
+++ EJS/index.js
@@ -2,8 +2,6 @@ import express from "express";
 import ejs from "ejs"
 import exp from "constants";
 
-const app = express();
-const PORT = 3000;
 
 app.listen(PORT,()=>{
     console.log(`Listening on ${PORT}`)
