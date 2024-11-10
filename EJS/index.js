2053a05103f5be6f0e6129456a7c41cfff89b149
Midhun Mathew <midmat4828@gmail.com>
2024-11-11 00:01:47 +0530

    TEST

--- EJS/index.js
+++ EJS/index.js
@@ -5,9 +5,6 @@ import exp from "constants";
 const app = express();
 const PORT = 3000;
 
-app.listen(PORT,()=>{
-    console.log(`Listening on ${PORT}`)
-}) 
 
 const dayName = new Date();
 const day = dayName.getDay()
