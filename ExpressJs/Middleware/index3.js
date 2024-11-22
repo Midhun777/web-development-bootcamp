a97d708e8a30cf62614e393c9588edd249d5118b
Midhun Mathew <midmat4828@gmail.com>
2024-11-22 14:30:00 +0530

    TEST

--- ExpressJs/Middleware/index3.js
+++ ExpressJs/Middleware/index3.js
@@ -8,11 +8,6 @@ function logger(req, res, next) {
   console.log("Request URL : ", req.url);
   next();
 }
-function logger(req, res, next) {
-  console.log("Request Method : ", req.method);
-  console.log("Request URL : ", req.url);
-  next();
-}
 
 app.use(logger);
 
