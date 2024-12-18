b35f6772c0adac05549cf06166c912fd2e901b45
Midhun Mathew <midmat4828@gmail.com>
2024-12-18 23:32:37 +0530

    EJS

--- EJS/index.js
+++ EJS/index.js
@@ -2,29 +2,42 @@ import express from "express";
 
 const app = express();
 const PORT = 3000;
-let type;
-let adv;
 
 app.get("/", (req, res) => {
-    const today = new Date();
-    const day = today.getDay();
-    console.log(day);
-
-    if (day == 0 || day == 6) {
-        type = "the weekend";
-        adv = "have some fun!";
-    } else {
-        type = "a week day";
-        adv = "Time to work hard!";
-    }
-
-    res.render("index.ejs", {
-        dayType: type,
-        advice: adv
-    });
+    const data = {
+        title: "Ejs tags",
+        seconds: new Date().getSeconds(),
+        items: ["apple", "orange", "banana"],
+        htmlContent: "<strong>This is some strong text</strong>"
+    };
+    res.render("index.ejs", data)
 });
+
 
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
 }
 )
+
+
+// let type;
+// let adv;
+
+// app.get("/", (req, res) => {
+//     const today = new Date();
+//     const day = today.getDay();
+//     console.log(day);
+
+//     if (day == 0 || day == 6) {
+//         type = "the weekend";
+//         adv = "have some fun!";
+//     } else {
+//         type = "a week day";
+//         adv = "Time to work hard!";
+//     }
+
+//     res.render("index.ejs", {
+//         dayType: type,
+//         advice: adv
+//     });
+// });
